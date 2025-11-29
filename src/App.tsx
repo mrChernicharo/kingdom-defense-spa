29c9b375a016fadc5d16d0eb0f009ebc07efd06d
Felipe Melo Chernicharo <felipe.melo_chernicharo@genesys.com>
2025-11-29 12:42:34 -0300

    zustand setup

---
+++ src/App.tsx
@@ -0,0 +1,57 @@
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+import { create } from "zustand";
+
+type BearState = {
+  bears: number;
+  deep: { nested: { name: string } };
+  increasePopulation: () => void;
+  removeAllBears: () => void;
+  setDeepNestedName: (newName: string) => void;
+};
+
+const useBear = create<BearState>()((set) => ({
+  bears: 0,
+  deep: { nested: { name: "Default" } },
+  increasePopulation: () => set((state) => ({ bears: state.bears + 1 })),
+  removeAllBears: () => set({ bears: 0 }),
+  setDeepNestedName: (newName: string) =>
+    set((state) => ({
+      ...state,
+      deep: { ...state.deep, nested: { ...state.deep.nested, name: newName } },
+    })),
+  // updateBears: (newBears) => set({ bears: newBears }),
+}));
+
+function App() {
+  const bears = useBear((state) => state.bears);
+  const name = useBear((state) => state.deep.nested.name);
+  const increasePopulation = useBear((state) => state.increasePopulation);
+  const removeAllBears = useBear((state) => state.removeAllBears);
+  const setDeepNestedName = useBear((state) => state.setDeepNestedName);
+
+  return (
+    <div>
+      <img src={reactLogo} />
+      <img src={viteLogo} />
+
+      <output>{name}</output>
+      <br />
+      <input
+        type="text"
+        value={name}
+        onChange={(ev) => setDeepNestedName(ev.target.value)}
+      />
+      <br />
+      <output>{bears}</output>
+      <br />
+
+      <button onClick={increasePopulation}>increase</button>
+      <button onClick={removeAllBears}>reset</button>
+    </div>
+  );
+}
+
+export default App;
